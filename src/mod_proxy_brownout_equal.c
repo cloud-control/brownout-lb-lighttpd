bc6bac706c384cc6fe2729cdcf77d83a6077ae35
Cristian KLEIN <cristiklein@gmail.com>
2014-03-10 08:25:48 +0100

    Actually implemented equal-thetas

--- src/mod_proxy_brownout_equal.c
+++ src/mod_proxy_brownout_equal.c
@@ -3,26 +3,21 @@
 #define data_proxy data_fastcgi
 
 void mod_proxy_brownout_equal_update_queue_offsets(data_array *extension, float dt) {
-	// TODO: change from diff to equal
-
 	/* Here we try to implement the following Python code:
 		dt = self.sim.now - self.lastDecision
 		if dt > 1: dt = 1
-
 		for i in range(0,len(self.backends)):
 			# Gain
-			Kp = 0.25
-			Ti = 5.0
-			gammaTr = .01
-
-			# PI control law
-			e = self.lastThetas[i] - self.lastLastThetas[i]
-			self.queueOffsets[i] += (Kp * e + (Kp/Ti) * self.lastThetas[i]) * dt
-
+			gamma = .1 * dt
+			gammaTr = .01 * dt
+			
+			# Calculate the negative deviation from the average
+			e = self.lastThetas[i] - avg(self.lastThetas)
+			# Integrate the negative deviation from the average
+			self.queueOffsets[i] += gamma * e
 			# Anti-windup
-			self.queueOffsets[i] -= gammaTr * (self.queueOffsets[i] - self.queueLengths[i]) * dt
+			self.queueOffsets[i] -= gammaTr * (self.queueOffsets[i] - self.queueLengths[i])
 			self.lastThetaErrors[i] = e
-
 		self.lastDecision = self.sim.now
 		
 		# choose replica with shortest (queue + queueOffset)
@@ -33,9 +28,18 @@ void mod_proxy_brownout_equal_update_queue_offsets(data_array *extension, float 
 	int numReplicas = (int) extension->value->used;
 	int i;
 
-	float Kp = 0.5;
-	float Ti = 5.0;
-	float gammaTr = 0.01;
+	float gamma = .1 * dt;
+	float gammaTr = .01 * dt;
+
+	float avgTheta = 0;
+	/* Compute average of thetas */
+	for (i = 0; i < numReplicas; i++) {
+		data_proxy *host = (data_proxy *)extension->value->data[i];
+		float lastTheta = host->lastTheta;
+
+		avgTheta += lastTheta;
+	}
+	avgTheta /= numReplicas;
 
 	/* Do control stuff */
 	for (i = 0; i < numReplicas; i++) {
@@ -45,11 +49,11 @@ void mod_proxy_brownout_equal_update_queue_offsets(data_array *extension, float 
 		int queueLength = host->usage;
 		float *pQueueOffset = &host->queueOffset;
 
-		// PI control law
-		float e = lastTheta - lastLastTheta;
-		*pQueueOffset += (Kp * e + (Kp/Ti) * lastTheta) * dt;
-
+		// Calculate the negative deviation from the average
+		float e = lastTheta - avgTheta;
+		// Integrate the negative deviation from the average
+		*pQueueOffset += gamma * e;
 		// Anti-windup
-		*pQueueOffset -= gammaTr * (*pQueueOffset - queueLength) * dt;
+		*pQueueOffset -= gammaTr * (*pQueueOffset - queueLength);
 	}
 }
